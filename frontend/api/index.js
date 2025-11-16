237acad9273cff8b3f5dc59d046b74b825d456f6
[Mohan krishnan O] <81806134+Mohankrish08@users.noreply.github.com>
2025-11-16 21:37:14 +0530

    updated: configure ssr deploy

--- frontend/api/index.js
+++ frontend/api/index.js
@@ -1,2 +1,4 @@
-export default import('../dist/safezone-ui/server/server.mjs')
-  .then(module => module.app());
+export default async (req, res) => {
+  const { reqHandler } = await import('../dist/safezone-ui/server/server.mjs');
+  return reqHandler(req, res);
+};
